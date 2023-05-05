44306809192762ab4cc628a5b0892aa5868c0d1c
vova2092 <vovai2092@yandex.ru>
2023-05-05 21:49:46 +0300

    Refactor in WeatherDaysCallbackFactory refactor type of days to Literal. Refactor callback_handlers.py

--- weather_tgbot_aiogram/handlers/callback_handlers.py
+++ weather_tgbot_aiogram/handlers/callback_handlers.py
@@ -2,7 +2,6 @@ from aiogram import Router, types
 from keyboards.inlinekeyboard import WeatherDaysCallbackFactory
 from services.get_format_weather import (format_weather_n_days,
                                          format_weather_now)
-from services.get_weather_n_days_old import get_weather_n_days
 
 STRING_FOR_STRIP = "Выбери период, за который показать погоду в городе:"
 
@@ -16,16 +15,10 @@ async def get_weather_n_days_handler(callback: types.CallbackQuery,
 
     city_from_callback = callback.message.text.strip(STRING_FOR_STRIP)
 
-    if callback_data.days == 0:
+    if callback_data.days == "now":
         await callback.message.answer(format_weather_now(city_from_callback))
-    elif callback_data.days == 1:
+    elif callback_data.days == "n_days":
         await callback.message.answer(
             format_weather_n_days(city_from_callback, callback_data.interval))
-    elif callback_data.days == 3:
-        await callback.message.answer(
-            get_weather_n_days(city_from_callback, callback_data.interval))
-    elif callback_data.days == 5:
-        await callback.message.answer(
-            get_weather_n_days(city_from_callback, callback_data.interval))
 
     await callback.answer()
--- weather_tgbot_aiogram/keyboards/inlinekeyboard.py
+++ weather_tgbot_aiogram/keyboards/inlinekeyboard.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Literal, Optional
 
 from aiogram.filters.callback_data import CallbackData
 from aiogram.types import InlineKeyboardMarkup
@@ -6,20 +6,20 @@ from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 
 class WeatherDaysCallbackFactory(CallbackData, prefix='get_weather'):
-    days: int
+    days: Literal["now"] | Literal["n_days"]
     interval: Optional[int]
 
 
 def get_inline_weather_keyboard() -> InlineKeyboardMarkup:
     keyboard = InlineKeyboardBuilder()
     keyboard.button(text="Текущая погода за окном",
-                    callback_data=WeatherDaysCallbackFactory(days=0)),
+                    callback_data=WeatherDaysCallbackFactory(days="now")),
     keyboard.button(text="Погода на день вперед",
-                    callback_data=WeatherDaysCallbackFactory(days=1, interval=8)),
+                    callback_data=WeatherDaysCallbackFactory(days="n_days", interval=8)),
     keyboard.button(text="Погода на 3 дня вперед",
-                    callback_data=WeatherDaysCallbackFactory(days=1, interval=24)),
+                    callback_data=WeatherDaysCallbackFactory(days="n_days", interval=24)),
     keyboard.button(text="Погода на 5 дней вперед",
-                    callback_data=WeatherDaysCallbackFactory(days=1, interval=40)),
+                    callback_data=WeatherDaysCallbackFactory(days="n_days", interval=40)),
     # builder.button(text="К начальному меню", callback_data=WeatherDaysCallbackFactory(days=1)),
     keyboard.adjust(1)
     return keyboard.as_markup()
